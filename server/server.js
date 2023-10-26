232c55351d02d9aff00bc8cd76096af961a61de0
khawarizmi <khawarizmijefri02@gmail.com>
2023-10-26 16:11:25 +0800

    adding schemas and add more package

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const mongodb = require('mongoose')
-require('dotenv').config()
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
 const PORT = 5000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.use(cors());
 
-app.listen(PORT , console.log(`Server run on PORT ${PORT}`))
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+mongoose.connect(process.env.MONGODB_CONNECT);
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () =>
+  console.log(`MongoDB connected on ${process.env.MONGODB_CONNECT}`)
+);
+
+app.use(express.json());
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
