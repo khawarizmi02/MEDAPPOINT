5d3d798e3d3961fc91f2ceece3235ab115cb983e
khawarizmi <khawarizmijefri02@gmail.com>
2023-10-26 14:36:23 +0800

    update on package.json and refine main page

--- client/app/page.js
+++ client/app/page.js
@@ -4,13 +4,17 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="bg-yellow-50">
+      <div className="font-bold text-[50px] py-3 text-center w-full bg-green-100">
+        Medappoint
+      </div>
+
+      <div className="bg-yellow-50 py-3 my-3 px-3">
         <Link href="/book"> Booking Page </Link>
       </div>
-      <div className="bg-blue-50">
+      <div className="bg-blue-50 py-3 my-3 px-3">
         <Link href="/queue"> Queue Page </Link>
       </div>
-      <div className="bg-red-50">
+      <div className="bg-red-50 py-3 my-3 px-3">
         <Link href="/profile"> Profile Page </Link>
       </div>
     </div>
