fe155ea86ae195fe4bb945f1f5c21ba0c9398c6b
alhakimm <alhakim1519@gmail.com>
2023-11-07 00:32:44 +0800

    buang comment page.js

--- client/app/page.js
+++ client/app/page.js
@@ -20,36 +20,6 @@ export default function Home() {
 					</div>
 					
 				</div>
-
-			<div className="font-bold text-[50px] py-3 text-center w-full bg-green-100">
-				Medappoint
-			</div>
-
-			<div className="bg-yellow-50 py-3 my-3 px-3">
-				<Link href="/booking"> Booking Page </Link>
-			</div>
-			<div className="bg-blue-50 py-3 my-3 px-3">
-				<Link href="/queue"> Queue Page </Link>
-			</div>
-			<div className="bg-red-50 py-3 my-3 px-3">
-				<Link href="/profile"> Profile Page </Link>
-			</div>
-			<div className="bg-red-50 py-3 my-3 px-3">
-				<Link href="/history"> History Page </Link>
-			</div>
-			<div className="bg-red-50 py-3 my-3 px-3">
-				<Link href="/post-report"> Post Report Page </Link>
-			</div>
-			<div className="bg-red-50 py-3 my-3 px-3">
-				<Link href="/queue"> Queue Page </Link>
-			</div> */}
-
-			{/* <div className="bg-red-50 py-3 my-3 px-3">
-				{loading ? null :
-					session
-						? <div>Signed in as {session.user.email}</div>
-						: <div>Not signed in</div>}
-			</div>
-		</div>
-	);
-}
+				</div>
+	)
+	}
