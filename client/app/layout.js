af948e90ed9f96b181f598343fc5a37bb9322bc5
SaffuanSaffery <saffuansaffery@gmail.com>
2023-11-08 16:50:47 +0800

    noty + queque

--- client/app/layout.js
+++ client/app/layout.js
@@ -54,68 +54,15 @@ export default function RootLayout({ children }) {
 				<Link href="/profile">Profile</Link>
 				<Link href="/post-report">Post Report</Link>
 				<Link href="/history">History</Link>
-				<a onClick={togglePopup}>Queue</a>
-				<a onClick={toggleNotification}>Notification</a>
+				<a onClick={togglePopup} className='hover: cursor-pointer'>Q/N</a>
 				<button className='rounded-lg bg-[#df0000] p-1 px-3 text-white'><Link href="/booking">My Booking</Link></button>
 				</div>
 			</nav>
         {children}
-
-          {popupVisible && (
-        		<div id="mypopup" className='fixed inset-0 bg-gray-500 bg-opacity-75 z-10' >
-          			<div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
-						      <div id="mypopup" className='relative z-10' aria-labelledby="modal-title" role="dialog" aria-modal="true">
-
-							    <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity'></div>
-							    <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
-								    <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
-									    <div className='relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
-
-                        <div className='flex flex-row justify-end'>
-                          <button type="button" className='bg-white rounded-md p-2 text-gray-400 hover:text-gray-500 hover:bg-gray-100' onClick={togglePopup}>
-                          <svg className='h-6 w-6' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                          </svg>
-                          </button>
-                        </div>
-
-                        <section className='flex flex-col items-center'>
-                          <h2 className='font-bold tracking-widest text-2xl'>{queueList[0]}</h2>
-                          <span className='text-sm leading-6 text-gray-600'>Current Turn</span>
-
-                          <h2 className='pt-5 font-bold tracking-widest text-2xl'>7323</h2>
-                          <span className='text-sm leading-6 text-gray-600'>Your Turn</span>
-                        </section>
-
-                        <section className='grid grid-cols-3 py-3'>
-                          <div className='flex flex-col items-center'>
-                            <h2 className=''>{queueList.length}</h2>
-                            <span className='ml-3 text-sm leading-6 text-gray-600'>person ahead of you</span>
-                          </div>
-
-                          <div className='flex flex-col items-center'>
-                            <h2 className=''> ~ {queueList.length/2}</h2>
-                            <span className='ml-3 text-sm leading-6 text-gray-600'>hours estimated</span>
-                          </div>
-
-                          <div className='flex flex-col items-center'>
-                            <h2 className=''>3</h2>
-                            <span className='ml-3 text-sm leading-6 text-gray-600'>room number</span>
-                          </div>
-                        </section>
-								
-									    </div>
-								    </div>
-							    </div>
-							  </div>
-          		</div>
-        		</div>
-     		  )}
-
-          {notiPopup && (
-            <div id="notipopup" className='fixed inset-0 bg-gray-500 bg-opacity-75 z-10' >
-              <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
-                <div id="notipopup" className='relative z-10' aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        {popupVisible && notiPopup && (
+          <div id="mypopup" className='fixed inset-0 bg-gray-500 bg-opacity-75 z-10' >
+            <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+              <div id="mypopup" className='relative z-10' aria-labelledby="modal-title" role="dialog" aria-modal="true">
 
                 <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity'></div>
                 <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
@@ -123,35 +70,86 @@ export default function RootLayout({ children }) {
                     <div className='relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
 
                       <div className='flex flex-row justify-end'>
-                        <button type="button" className='bg-white rounded-md p-2 text-gray-400 hover:text-gray-500 hover:bg-gray-100' onClick={toggleNotification}>
-                        <svg className='h-6 w-6' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
+                        <button type="button" className='bg-white rounded-md p-2 text-gray-400 hover:text-gray-500 hover:bg-gray-100' onClick={togglePopup}>
+                          <svg className='h-6 w-6' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                          </svg>
                         </button>
                       </div>
 
                       <section className='flex flex-col items-center'>
-                        <h2 className='ml-3 text-sm leading-6 text-gray-600'>Upcoming appointment at: </h2>
-                        <h2 className='font-bold tracking-wide text-2xl'>{hospital}</h2>
+                        <h2 className='font-bold tracking-widest text-2xl'>{queueList[0]}</h2>
+                        <span className='text-sm leading-6 text-gray-600'>Current Turn</span>
 
-                        {/* <h2 className='ml-3 text-sm leading-6 text-gray-600'>Be there before </h2>
-                        <h2 className='font-bold tracking-wide text-2xl'>{booking.bookTime}</h2> */}
+                        <h2 className='pt-5 font-bold tracking-widest text-2xl'>7323</h2>
+                        <span className='text-sm leading-6 text-gray-600'>Your Turn</span>
                       </section>
 
-                      <section className='flex flex-row justify-center py-5'>
-                        <a onClick={toggleNotification}><span class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-l font-medium text-red-700 ring-1 ring-inset ring-red-600/10 hover:cursor-pointer">Cancel</span></a>
-                        <a onClick={toggleNotification}><span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-l font-medium text-green-700 ring-1 ring-inset ring-green-600/20 hover:cursor-pointer">Accept</span></a>
-                      </section>
+                      <section className='grid grid-cols-3 py-3'>
+                        <div className='flex flex-col items-center'>
+                          <h2 className=''>{queueList.length}</h2>
+                          <span className='ml-3 text-sm leading-6 text-gray-600'>person ahead of you</span>
+                        </div>
 
-                      </div>
+                        <div className='flex flex-col items-center'>
+                          <h2 className=''> ~ {queueList.length/2}</h2>
+                          <span className='ml-3 text-sm leading-6 text-gray-600'>hours estimated</span>
+                        </div>
+
+                        <div className='flex flex-col items-center'>
+                          <h2 className=''>3</h2>
+                          <span className='ml-3 text-sm leading-6 text-gray-600'>room number</span>
+                        </div>
+                      </section>
+                  
                     </div>
                   </div>
-
                 </div>
               </div>
             </div>
-          )}
+          </div>
+        )}
 
+        {popupVisible && !notiPopup && (
+          <div id="notipopup" className='fixed inset-0 bg-gray-500 bg-opacity-75 z-10' >
+            <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+              <div id="notipopup" className='relative z-10' aria-labelledby="modal-title" role="dialog" aria-modal="true">
+
+              <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity'></div>
+              <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
+                <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+                  <div className='relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
+
+                    <div className='flex flex-row justify-end'>
+                      <button type="button" className='bg-white rounded-md p-2 text-gray-400 hover:text-gray-500 hover:bg-gray-100' onClick={togglePopup}>
+                        <svg className='h-6 w-6' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                      </button>
+                    </div>
+
+                    <section className='flex flex-col items-center'>
+                      <h2 className='ml-3 text-sm leading-6 text-gray-600'>Upcoming appointment at: </h2>
+                      <h2 className='font-bold tracking-wide text-2xl'>{hospital}</h2>
+
+                      {/* <h2 className='ml-3 text-sm leading-6 text-gray-600'>Be there before </h2>
+                          <h2 className='font-bold tracking-wide text-2xl'>{booking.bookTime}</h2> */}
+                    </section>
+
+                    <section className='flex flex-row justify-center py-5'>
+                      <a onClick={togglePopup}><span class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-l font-medium text-red-700 ring-1 ring-inset ring-red-600/10 hover:cursor-pointer">Cancel</span></a>
+                      <a onClick={toggleNotification}><span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-l font-medium text-green-700 ring-1 ring-inset ring-green-600/20 hover:cursor-pointer">Accept</span></a>
+                    </section>
+
+                  </div>
+                </div>
+
+              </div>
+              </div>
+            </div>
+          </div>
+        )}
+          
       </body>
     </html>
   )
